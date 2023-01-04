f560774110e9cead8c8c14994ab851d67f57212e
GeertUU <121939243+GeertUU@users.noreply.github.com>
2023-01-04 14:56:34 +0100

    Update comment to correct package name

--- imageAnalysis/__init__.py
+++ imageAnalysis/__init__.py
@@ -4,7 +4,7 @@ from .coordinatefilemaniputlations import coordinateFileManipulation
 from .calculatesann import calculateSANN, fromfileSANN
 from .imagesclass import realspace, realspacefromfile, Fourier, imageframes
 
-#make visible for 'from scm_electron_microscopes import *'
+#make visible for 'from imageAnalysis import *'
 __all__ = [
     'coordinateFileManipulation',
     'calculateSANN',
