62eea448ba3838f686b997f40acf2aa8244501f2
GeertUU <121939243+GeertUU@users.noreply.github.com>
2023-01-04 16:51:39 +0100

    fix(?) "from imageAnalysis import *"

--- imageAnalysis/__init__.py
+++ imageAnalysis/__init__.py
@@ -1,16 +1,16 @@
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
-#from .coordinatefilemaniputlations import coordinateFileManipulation
-#from .calculatesann import calculateSANN, fromfileSANN
-#from .imagesclass import realspace, realspacefromfile, Fourier, imageframes
+from .coordinatefilemaniputlations import coordinateFileManipulation
+from .calculatesann import calculateSANN, fromfileSANN
+from .imagesclass import realspace, realspacefromfile, Fourier, imageframes
 
 #make visible for 'from imageAnalysis import *'
 __all__ = [
-    '.coordinatefilemaniputlations.coordinateFileManipulation',
-    '.calculatesann.calculateSANN',
-    '.calculatesann.fromfileSANN',
-    '.imagesclass.realspace',
-    '.imagesclass.realspacefromfile',
-    '.imagesclass.Fourier',
-    '.imagesclass.imageframes',
+    'coordinateFileManipulation',
+    'calculateSANN',
+    'fromfileSANN',
+    'realspace',
+    'realspacefromfile',
+    'Fourier',
+    'imageframes',
 ]
