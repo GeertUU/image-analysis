fd6688196757b19c59ce2979daf5332a335d1a2e
GeertUU <g.h.a.schulpen@uu.nl>
2023-01-10 10:20:29 +0100

    correct the radialprofile method for realspace use

--- imageanalysis/__init__.py
+++ imageanalysis/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 from imageanalysis.coordinatefilemaniputlations import CoordinateFileManipulation
 from imageanalysis.calculatesann import CalculateSANN, FromFileSANN
